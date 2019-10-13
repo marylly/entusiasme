cb01826127b5a712f5d6fc41d28e38e0eba3b0e5
Simone <monymachad@yahoo.com.br>
2019-10-13 15:22:59 -0300

    Organizing inside a variable the kotlin version to be reused

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion: String by extra { "1.2.4" }
+val kotlinVersion: String by extra { "1.3.50" }
 
 buildscript {
-    extra.set("kotlinVersion", "1.3.50")
-
+	val kotlinVersion: String by extra { "1.3.50" }
+	
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra.get("kotlinVersion")}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
 plugins {
-	val kotlinVersion = "1.3.50"
-	
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
 
     application
@@ -29,8 +28,9 @@ repositories {
 dependencies {
 	implementation("org.slf4j:slf4j-api:1.7.2")
 	implementation("org.slf4j:slf4j-simple:1.7.2")
-	
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 	implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
